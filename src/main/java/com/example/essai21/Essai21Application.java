4f16fc16f9b0361c1d61f36de204503502b3ebef
charroux <benoit.charroux@gmail.com>
2024-10-14 15:27:26 +0200

    first commit

---
+++ src/main/java/com/example/essai21/Essai21Application.java
@@ -0,0 +1,13 @@
+package com.example.essai21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Essai21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Essai21Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/essai21/Essai21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.essai21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Essai21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
